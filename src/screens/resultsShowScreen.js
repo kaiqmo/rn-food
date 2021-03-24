ccacbeebb84d5f67074f52d1c2d01b17ca47074a
kaique <kaiquesilva@codeby.com.br>
2021-03-24 19:43:08 -0300

    feature Show one result screen

--- src/screens/resultsShowScreen.js
+++ src/screens/resultsShowScreen.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import {View,Text,Stylesheet} from 'react-native';
+import React, {useState,useEffect} from 'react';
+import {View,Text,StyleSheet,FlatList,Image} from 'react-native';
+
+import yelp from '../api/yelp';
 
 const resultsShowScreen =({navigation}) =>{
+    const [result,setResult] = useState(null);
     const id = navigation.getParam('id');
+    console.log(result);
+    const getResult = async(id) =>{
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data); 
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, [])
+    if(!result){
+        return null;
+    }
     return(
         <View>
-            <Text>resultsShow {id}</Text>
+            {/* <Image source={{uri: result.image_url}}/> */}
+            <Text>{result.name}</Text>
+            <FlatList 
+                data={result.photos}
+                keyExtractor={ (photo)=> photo}
+                renderItem={ ({item})=>{
+                    return <Image style={styles.imageStyles} source={{uri: item}} />
+                }}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    imageStyles:{
+        width:150,
+        height:150
+    },
+});
 
 export default resultsShowScreen;
