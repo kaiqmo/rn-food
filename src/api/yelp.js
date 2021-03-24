d3861115766803c11fed5b05b6157643f97e2e48
kaique <kaiquesilva@codeby.com.br>
2021-03-24 17:42:26 -0300

    useEffect,Yelp api

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://api.yelp.com/v3/businesses',
+    headers:{
+        Authorization:'Bearer mhKVyWFwM7I3NZkltfh0i2B1h7gm94THgPPgaMCZKUBRLnXKT7HoWYULDminFLrQNglHbMc2L4WL3CmIQl8cccBf8_7mS9BijMotlMFkgTS61EVN6Os4skxyp05aYHYx',
+    }
+});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Text, View, StyleSheet} from 'react-native';
+import yelp from '../api/yelp';
 import SearchBar from '../components/searchbar';
+
+
+
 
 const SearchScreen = () =>{
     const [term, setTerm] = useState('');
-    console.log(term);
+    const [results,setResults] = useState([]);
+    const [errorMessage,setErrorMessage] = useState('');
+
+    const searchApi = async(searchTerm) =>{
+        try{
+        const response = await yelp.get('/search',{
+            params:{
+                limit:50,
+                term:searchTerm,
+                location:'sao paulo'
+            }
+        });
+        setResults(response.data.businesses);
+        }catch(e){
+            console.log('error',e);
+            setErrorMessage('Something went Wrong');
+        }
+    };
+
+    // call searchApi when component is rendered
+    // useEffect can be used to call the function only when the page starts! or multiple times depending on the arary
+    useEffect(()=>{
+        searchApi('pasta');
+    },[])
+
     return(
         <View style={styles.viewStyle}>
             <SearchBar 
                 term={term} 
                 onTermChange={ (newTerm) =>setTerm(newTerm)}
-                onTermSubmit={()=>console.log('done')} />
+                onTermSubmit={()=>searchApi(term)} />
+            {
+                errorMessage > 0 ?
+                <Text>{errorMessage}</Text>
+                : null
+            }
+            <Text>We found {results.length} results</Text>
         </View>
     );
 }
